0afb5020e38a14489159655b47eecf010ec0dd7c
Evgeny Kholukhoev <eekholukhoev@edu.hse.ru>
2018-05-01 00:16:29 +0300

    Extra task - 2

--- src/main/java/ru/sbt/jschool/session1/ExtraTasks.java
+++ src/main/java/ru/sbt/jschool/session1/ExtraTasks.java
@@ -2,9 +2,8 @@ package ru.sbt.jschool.session1;
 
 public class ExtraTasks {
     public static void main(String[] args) {
-        System.out.println(longValueOf("123A", 16));
-        System.out.println(longValueOf("1000", 2));
-        System.out.println(longValueOf("0", 2));
+        System.out.println(sumOf("1", "101", 2));
+        System.out.println(sumOf("1", "1", 3));
     }
 
     private static long longValueOf(String s, int radix) {
@@ -14,16 +13,35 @@ public class ExtraTasks {
             while (len > i) {
                 int digit = Character.digit(s.charAt(i++), radix);
                 if (digit < 0) {
-                    throw new NumberFormatException(String.format("for input string %s", s));
+                    throw new NumberFormatException(String.format("for input string %s.", s));
                 }
                 result *= radix;
                 result += digit;
             }
         } else {
-            throw new NumberFormatException(String.format("for input string %s", s));
+            throw new NumberFormatException(String.format("for input string %s.", s));
         }
         return result;
     }
 
+    private static long sumOf(String s1, String s2, int radix) {
+        long result = 0;
+        int len1 = s1.length(), len2 = s2.length();
+        int len = len1 > len2 ? len1 : len2;
+        int i = 0;
+        int mult = 1;
+        if (len > 0) {
+            while (i < (len)) {
+                int digit1 = i < len1 ? Character.digit(s1.charAt(len1 - 1 - i), radix) : 0;
+                int digit2 = i < len2 ? Character.digit(s2.charAt(len2 - 1 - i), radix) : 0;
+                i++;
+                result += (digit1 + digit2) * mult;
+                mult *= radix;
+            }
+        } else {
+            throw new NumberFormatException(String.format("for input strings: %s and %s.", s1, s2));
+        }
+        return result;
+    }
 
 }
